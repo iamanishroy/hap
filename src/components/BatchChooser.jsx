06cbea890ad16983ce9d184181e9fd1764df35d2
Anish Roy <62830866+Anish6275@users.noreply.github.com>
2021-03-27 21:58:32 +0530

    Batch form BackEnd

--- src/components/BatchChooser.jsx
+++ src/components/BatchChooser.jsx
@@ -1,48 +1,68 @@
 import React from "react";
-import axios from "axios";
+import Request from "axios-react";
 import "../style/BatchChooser.css";
-const fet = () => {
-  // var data = JSON.stringify({
-  //   name: "Alpha",
-  //   to: "anishroy212@gmail.com",
-  //   subject: "test!!!",
-  //   emailBody: "HELLO WORLD",
-  // });
+// const fet = () => {
+//   // var data = JSON.stringify({
+//   //   name: "Alpha",
+//   //   to: "anishroy212@gmail.com",
+//   //   subject: "test!!!",
+//   //   emailBody: "HELLO WORLD",
+//   // });
 
-  // var config = {
-  //   method: "post",
-  //   url: "https://fantasy-quickest-child.glitch.me/batch",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   // data: data,
-  // };
+//   var config = {
+//     method: "post",
+//     url: "https://fantasy-quickest-child.glitch.me/batch",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     // data: data,
+//   };
 
-  axios
-    .get("https://fantasy-quickest-child.glitch.me/batch")
-    .then(function (response) {
-      console.log(JSON.stringify(response.data));
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-};
+//   axios(config)
+//     .then(function (response) {
+//       console.log(JSON.stringify(response.data));
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     });
+// };
 const BatchChooser = () => {
   return (
     <>
-      <div className="optionsContainer">
-        <div className="options">
-          <div className="option" onClick={fet}>
-            BCA Sem II
-          </div>
-          <div className="option">BCA Sem IV</div>
-          <div className="option">BCA Sem VI</div>
-          <div className="option">BCA Sem II</div>
-          <div className="option">BCA Sem IV</div>
-          <div className="option">BCA Sem VI</div>
-          <div className="option">BCA Sem II</div>
-        </div>
-      </div>
+      <Request
+        config={{
+          method: "post",
+          url: "https://fantasy-quickest-child.glitch.me/batch",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          // data: data,
+        }}
+      >
+        {({ loading, response, error, refetch, networkStatus }) => (
+          <>
+            {loading ? (
+              <>
+                <span>FETCHING...</span>
+              </>
+            ) : (
+              <></>
+            )}
+            {!loading && error && <p>error: {error.response.data.errors[0]}</p>}
+            <div className="optionsContainer">
+              <div className="options">
+                {!loading &&
+                  response &&
+                  Object.keys(response.data).map((id) => (
+                    <>
+                      <div className="option">{response.data[id]}</div>
+                    </>
+                  ))}
+              </div>
+            </div>
+          </>
+        )}
+      </Request>
     </>
   );
 };
